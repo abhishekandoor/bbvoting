714a2bdba94132a6dc92c68096213db7e674907f
abhishek <snabhishek2@gmail.com>
2024-03-11 00:46:40 +0530

    new changes

--- application/views/ml/all_contestants.php
+++ application/views/ml/all_contestants.php
@@ -34,7 +34,7 @@ foreach($contestants as $row){ ?>
 
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center card-type">
             <div class="d-flex py-2 align-items-center first_container">
-                <img src="https://cdn.pixabay.com/photo/2016/05/28/05/40/question-mark-1421017_960_720.png" alt="" class="rounded me-3 card-list-img" width="50">
+                <img src="<?php echo base_url().'/'.$row['photo_url']; ?>" alt="" class="rounded me-3 card-list-img" width="50">
                 <div class="me-3">
                     <p class="fs-14 mb-1">Name</p>
                     <span class=" font-w500 fs-16"><?php echo $row['name']; ?></span>
