38354bd3e3092f41a7c13d90e5af40fe9f40ebcb
harisharma <harisharma96@gmail.com>
2024-03-11 12:43:03 +0530

    changed added

--- application/views/template.php
+++ application/views/template.php
@@ -50,6 +50,21 @@
 
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9199579426500892" crossorigin="anonymous"></script>
 
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EN9085MVTP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-EN9085MVTP');
+    </script>
+
+
 </head>
 
 <body>
